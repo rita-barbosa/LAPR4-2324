6134e43f81bd7aaf65fac6792370312c7d36963b
Ana Guterres <1221933@isep.ipp.pt>
2024-04-03 21:32:02 +0100

    chore: add scripts to run backofficeApp and bootstrap
    [#6]

---
+++ run-backofficeApp.bat
@@ -0,0 +1,6 @@
+REM set the class path,
+REM assumes the build was executed with maven copy-dependencies
+SET BASE_CP=jobs4u.app.backoffice.console\target\jobs4u.app.backoffice.console-0.1.0.jar;jobs4u.app.backoffice.console\target\dependency\*;
+
+REM call the java VM, e.g,
+java -cp %BASE_CP% jobs4u.base.app.backoffice.console.BaseBackoffice
---
+++ run-bootstrap.bat
@@ -0,0 +1,6 @@
+REM set the class path,
+REM assumes the build was executed with maven copy-dependencies
+SET BASE_CP=jobs4u.bootstrap\target\jobs4u.bootstrap-0.1.0.jar;jobs4u.bootstrap\target\dependency\*;
+
+REM call the java VM, e.g,
+java -cp %BASE_CP% jobs4u.base.app.bootstrap.BaseBootstrap
