039c1b3e9b1a7c729724faaa45cbed18b88b8366
Ana Guterres <1221933@isep.ipp.pt>
2024-04-07 15:02:03 +0100

    fix: correcting the line ending
    [#6]

--- run-backoffice-app.sh
+++ run-backoffice-app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# set the class path,
+# set the class  path,
 # assumes the build was executed with maven copy-dependencies
 export BASE_CP=jobs4u.app.backoffice.console/target/jobs4u.app.backoffice.console-0.1.0.jar:jobs4u.app.backoffice.console/target/dependency/*;
 
