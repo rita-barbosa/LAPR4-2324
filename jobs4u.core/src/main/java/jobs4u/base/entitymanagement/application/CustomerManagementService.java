39e97f311cda637d5d5efa5480afeae68b134a63
rita_barbosa <1220841@isep.ipp.pt>
2024-05-01 22:22:08 +0100

    feat: adds controller and service for the registration of customers. [#89]

---
+++ jobs4u.core/src/main/java/jobs4u/base/entitymanagement/application/CustomerManagementService.java
@@ -0,0 +1,57 @@
+package jobs4u.base.entitymanagement.application;
+
+import eapli.framework.domain.events.DomainEvent;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.infrastructure.authz.application.UserManagementService;
+import eapli.framework.infrastructure.authz.domain.model.Role;
+import eapli.framework.infrastructure.authz.domain.model.SystemUser;
+import eapli.framework.infrastructure.authz.domain.model.Username;
+import eapli.framework.infrastructure.pubsub.EventPublisher;
+import eapli.framework.infrastructure.pubsub.impl.inprocess.service.InProcessPubSub;
+import jobs4u.base.entitymanagement.domain.CompanyName;
+import jobs4u.base.entitymanagement.domain.CustomerCode;
+import jobs4u.base.entitymanagement.domain.events.NewCustomerUserRegisteredEvent;
+import jobs4u.base.entitymanagement.dto.CustomerDTO;
+import jobs4u.base.entitymanagement.domain.Customer;
+import jobs4u.base.entitymanagement.repository.CustomerRepository;
+import jobs4u.base.infrastructure.persistence.PersistenceContext;
+import jobs4u.base.jobopeningmanagement.application.CustomerListDTOService;
+import jobs4u.base.jobopeningmanagement.domain.Address;
+import jobs4u.base.usermanagement.application.GeneratePasswordService;
+import jobs4u.base.usermanagement.domain.BaseRoles;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+public class CustomerManagementService {
+
+    private final CustomerListDTOService service = new CustomerListDTOService();
+    private final GeneratePasswordService passSvc = new GeneratePasswordService();
+
+    private final UserManagementService userSvc = AuthzRegistry.userService();
+    private final CustomerRepository customerRepository = PersistenceContext
+            .repositories().customers();
+
+    private final EventPublisher dispatcher = InProcessPubSub.publisher();
+
+    public void registerNewCustomer(String companyName, String address, String customerCode,
+                                    SystemUser customerManager, String email) {
+        String password = passSvc.generatePassword();
+        final Set<Role> roles = new HashSet<>();
+        roles.add(BaseRoles.CUSTOMER_USER);
+
+        SystemUser customerUser = userSvc.registerNewUser(email, password, companyName, " ", email, roles);
+
+        final DomainEvent event = new NewCustomerUserRegisteredEvent(new CompanyName(companyName),
+                new Address(address), new CustomerCode(customerCode), customerManager, customerUser);
+        dispatcher.publish(event);
+
+    }
+
+    public List<CustomerDTO> getAssignedCustomerCodesList(Username username) {
+        List<Customer> customersList = customerRepository.getCustomersByUsername(username);
+        return service.convertToDTO(customersList);
+    }
+}
---
+++ jobs4u.core/src/main/java/jobs4u/base/entitymanagement/application/RegisterCustomerController.java
@@ -0,0 +1,27 @@
+package jobs4u.base.entitymanagement.application;
+
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.infrastructure.authz.application.UserManagementService;
+import eapli.framework.infrastructure.authz.domain.model.SystemUser;
+import jobs4u.base.usermanagement.application.GeneratePasswordService;
+import jobs4u.base.usermanagement.domain.BaseRoles;
+
+import java.util.Optional;
+
+public class RegisterCustomerController {
+    private final AuthorizationService authz = AuthzRegistry.authorizationService();
+    private final UserManagementService userSvc = AuthzRegistry.userService();
+    private final CustomerManagementService custSvc = new CustomerManagementService();
+
+
+    public boolean registerNewCustomer(String companyName, String address, String customerCode,
+                                       String email) {
+        Optional<SystemUser> customerManager = authz.loggedinUserWithPermissions(BaseRoles.ADMIN, BaseRoles.CUSTOMER_MANAGER);
+
+        customerManager.ifPresent(systemUser -> custSvc.registerNewCustomer(companyName, address, customerCode, systemUser, email));
+
+
+        return true;
+    }
+}
