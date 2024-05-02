d9586697e4911ef98f6b99d2436221f922573fcc
Matilde Varela <1220683@isep.ipp.pt>
2024-05-02 21:18:48 +0100

    feat: US1002 persistence related classes update
    [#27]

---
+++ jobs4u.core/src/main/java/jobs4u/base/candidatemanagement/CandidateName.java
@@ -0,0 +1,13 @@
+package jobs4u.base.candidatemanagement;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class CandidateName {
+
+    private String candidateName;
+
+    public CandidateName() {
+
+    }
+}
