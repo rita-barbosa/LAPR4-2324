d1e91055c5bf785d511c647ae57443617f31721d
Ana Guterres <1221933@isep.ipp.pt>
2024-05-03 10:33:38 +0100

    feat: updates us_2001 implementation. change in copy_files function and add creation of the output directory to main.
    [#41]

--- scomp/us2001/copy_files.c
+++ scomp/us2001/copy_files.c
@@ -7,9 +7,9 @@
 
 void copy_files(int n, char *input_directory, char *output_directory){
     DIR *dir;
-    FILE *input_file, *output_file;
     struct dirent *entry;
     char subdirectory[15];
+    pid_t pid;
 
     sprintf(subdirectory, "candidate%d", n);
 
@@ -19,58 +19,40 @@ void copy_files(int n, char *input_directory, char *output_directory){
 
     // Open the directory
     dir = opendir(input_directory);
-    if (!dir)
+    if (!dir) 
     {
         perror("opendir");
         exit(EXIT_FAILURE);
     }
 
+    
+
     // Iterate over the directory entries
     while ((entry = readdir(dir)) != NULL)
     {
-
         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
             continue;
         }
 
-        if (entry->d_name[0] - '0' == n)
-        {
-            // Construct source file path
-            char source_file_path[257];
-            sprintf(source_file_path, "%s/%s", input_directory, entry->d_name);
-            
-            // Construct destination file path
-            char dest_file_path[272]; 
-            sprintf(dest_file_path, "%s/%s/%s", output_directory, subdirectory, entry->d_name);
-            
-            input_file = fopen(source_file_path, "rb");
-            if (input_file == NULL) {
-                perror("fopen");
-                exit(1);
-            }  
+        if (entry->d_name[0] - '0' == n) {
+            pid = fork();
 
-            // Open destination file for writing
-            output_file = fopen(dest_file_path, "wb");
-            if (output_file == NULL) {
-                perror("fopen");
-                exit(1);
-            } 
+            if(pid == 0){
+                char source_file_path[257];
+                sprintf(source_file_path, "%s/%s", input_directory, entry->d_name);
 
-            // Copy file contents
-            char buffer[10];
-            size_t bytes_read;
-            while ((bytes_read = fread(buffer, 1, sizeof(buffer), input_file)) > 0) {
-                fwrite(buffer, 1, bytes_read, output_file);
+                char dest_file_path[257];
+                sprintf(dest_file_path, "%s/%s", output_directory, subdirectory);
+
+                execlp("cp", "cp", source_file_path, dest_file_path, (char *)NULL);
+                perror("execlp");
+                exit(EXIT_FAILURE);
             }
-            
-            // Close files
-            fclose(input_file);
-            fclose(output_file);
-            
-            printf("»» COPY ALERT: The file %s from %s was copied to %s\n", entry->d_name, input_directory, subdirectory);
-        }
 
-}
+             printf("The file %s from %s was copied to %s\n", entry->d_name, input_directory, subdirectory);
+        }
+        
+    }
 
     // Close the directory
     closedir(dir);
