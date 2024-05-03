9c2fac45c6f6021e68b751507c72cbc43aa1ed58
Ana Guterres <1221933@isep.ipp.pt>
2024-05-03 12:16:50 +0100

    feat: us_1005 major domain classes setup.
    [#69]

---
+++ jobs4u.core/src/main/java/jobs4u/base/applicationmanagement/domain/RequirementAnswer.java
@@ -0,0 +1,38 @@
+package jobs4u.base.applicationmanagement.domain;
+
+import eapli.framework.domain.model.ValueObject;
+import eapli.framework.validations.Preconditions;
+
+import java.util.Objects;
+
+public class RequirementAnswer implements ValueObject {
+
+    private String requirementAnswer;
+
+    public RequirementAnswer(String answer){
+        Preconditions.noneNull(answer);
+        Preconditions.nonEmpty(answer);
+        this.requirementAnswer = answer;
+    }
+
+    public RequirementAnswer(){
+        //for ORM
+    }
+
+    public static RequirementAnswer valueOf(final String answer){
+        return new RequirementAnswer(answer);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RequirementAnswer)) return false;
+        RequirementAnswer that = (RequirementAnswer) o;
+        return Objects.equals(requirementAnswer, that.requirementAnswer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(requirementAnswer);
+    }
+}
