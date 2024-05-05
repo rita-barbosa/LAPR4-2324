d176f524b3f23f38ba8d49a28d7a0dd8c56de69b
Ana Guterres <1221933@isep.ipp.pt>
2024-05-05 19:37:48 +0100

    feat: updates us_1012 related classes.
    [#74]

--- jobs4u.app.backoffice.console/src/main/java/jobs4u/base/app/backoffice/console/presentation/operator/RegisterJobOpeningApplicationUI.java
+++ jobs4u.app.backoffice.console/src/main/java/jobs4u/base/app/backoffice/console/presentation/operator/RegisterJobOpeningApplicationUI.java
@@ -4,10 +4,12 @@ import eapli.framework.domain.repositories.ConcurrencyException;
 import eapli.framework.domain.repositories.IntegrityViolationException;
 import eapli.framework.io.util.Console;
 import eapli.framework.presentation.console.AbstractUI;
+import eapli.framework.presentation.console.SelectWidget;
 import jobs4u.base.applicationmanagement.application.RegisterJobOpeningApplicationController;
 import jobs4u.base.applicationmanagement.domain.Application;
 import jobs4u.base.applicationmanagement.domain.ApplicationFile;
 import jobs4u.base.candidatemanagement.domain.PhoneNumber;
+import jobs4u.base.jobopeningmanagement.dto.JobOpeningDTO;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -22,6 +24,32 @@ public class RegisterJobOpeningApplicationUI extends AbstractUI {
 
         File folder = new File(sharedFolderPath);
 
+        int index = 0;
+        List<JobOpeningDTO>  jobOpeningListDTO = controller.getJobOpeningsList();
+
+        for (JobOpeningDTO jobOpening: jobOpeningListDTO) {
+            System.out.printf("INDEX: %d", index);
+            String sb = "==================================================================\n" +
+                    "[Job Reference] " + jobOpening.getJobReference() + "\n" +
+                    "[Status] " + jobOpening.getStatus() + "\n" +
+                    "[Function] " + jobOpening.getFunction() + "\n" +
+                    "[Description] " + jobOpening.getDescription() + "\n" +
+                    "[Contract Type] " + jobOpening.getContractType() + "\n" +
+                    "[Work Mode] " + jobOpening.getWorkMode() + "\n" +
+                    "[Address] " + jobOpening.getAddress() + "\n" +
+                    "[Customer Code] " + jobOpening.getCustomerCode() + "\n" +
+                    "[Number of Vacancies] " + jobOpening.getNumVacancies() + "\n" +
+                    "[Requirement Specification] " + jobOpening.getRequirementName() + "\n" +
+                    "=====================================================================";
+            System.out.println(sb);
+            index++;
+        }
+
+        int option = selectsIndexNoCancel(jobOpeningListDTO);
+
+        JobOpeningDTO job = jobOpeningListDTO.get(option);
+
+
         if (folder.isDirectory()) {
             File[] files = folder.listFiles();
 
@@ -34,8 +62,12 @@ public class RegisterJobOpeningApplicationUI extends AbstractUI {
                             String name = scanner.nextLine();
                             String phoneNumber = scanner.nextLine();
 
+                            PhoneNumber number = new PhoneNumber("+351", phoneNumber);
 
-                            boolean validate = controller.registerCandidate(name, email, "+351", phoneNumber);
+                            boolean candidateExists = controller.getCandidate(number);
+                            if (!candidateExists){
+                                boolean validate = controller.registerCandidate(name, email, "+351", phoneNumber);
+                            }
 
                             Set<ApplicationFile> applicationFiles = new HashSet<>();
 
@@ -43,10 +75,8 @@ public class RegisterJobOpeningApplicationUI extends AbstractUI {
                                 applicationFiles.add(new ApplicationFile(otherFile));
                             }
 
-                            PhoneNumber number = new PhoneNumber("+351", phoneNumber);
-
                             try {
-                                Optional<Application> application = controller.registerApplication(applicationFiles, new Date(), number);
+                                Optional<Application> application = controller.registerApplication(applicationFiles, new Date(), number, job);
                                 if (application.isEmpty()) {
                                     throw new IntegrityViolationException();
                                 } else {
@@ -67,6 +97,21 @@ public class RegisterJobOpeningApplicationUI extends AbstractUI {
             System.out.println("Invalid directory: " + sharedFolderPath);
         }
         return true;
+    }
+
+    private static int selectsIndexNoCancel(List<?> list) {
+        String input;
+        int value;
+        do {
+            input = Console.readNonEmptyLine("Select the job opening index:", "Providing a job opening option is obligatory.");
+
+            try {
+                value = Integer.parseInt(input);
+            } catch (NumberFormatException ex) {
+                value = -1;
+            }
+        } while (value < 0 || value > list.size() - 1);
+        return value;
     }
 
     @Override
--- jobs4u.core/src/main/java/jobs4u/base/applicationmanagement/application/RegisterJobOpeningApplicationController.java
+++ jobs4u.core/src/main/java/jobs4u/base/applicationmanagement/application/RegisterJobOpeningApplicationController.java
@@ -5,19 +5,19 @@ import eapli.framework.infrastructure.authz.application.AuthorizationService;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
 import jobs4u.base.applicationmanagement.domain.Application;
 import jobs4u.base.applicationmanagement.domain.ApplicationFile;
-import jobs4u.base.applicationmanagement.repositories.ApplicationRepository;
 import jobs4u.base.candidatemanagement.application.RegisterCandidateController;
 import jobs4u.base.candidatemanagement.domain.Candidate;
 import jobs4u.base.candidatemanagement.domain.PhoneNumber;
 import jobs4u.base.candidatemanagement.repository.CandidateRepository;
 import jobs4u.base.infrastructure.persistence.PersistenceContext;
+import jobs4u.base.jobopeningmanagement.application.JobOpeningManagementService;
+import jobs4u.base.jobopeningmanagement.domain.JobOpening;
+import jobs4u.base.jobopeningmanagement.dto.JobOpeningDTO;
 import jobs4u.base.jobopeningmanagement.repositories.JobOpeningRepository;
 import jobs4u.base.usermanagement.domain.BaseRoles;
 
-import java.io.File;
-import java.util.Date;
-import java.util.Optional;
-import java.util.Set;
+
+import java.util.*;
 
 @UseCaseController
 public class RegisterJobOpeningApplicationController {
@@ -30,18 +30,46 @@ public class RegisterJobOpeningApplicationController {
 
     private final ApplicationManagementService applicationManagementService;
 
+    private final JobOpeningRepository jobOpeningRepository;
+
+    private final JobOpeningManagementService jobOpeningManagementService;
+
     public RegisterJobOpeningApplicationController(){
         this.authz = AuthzRegistry.authorizationService();
         this.applicationManagementService = new ApplicationManagementService();
+        this.jobOpeningManagementService = new JobOpeningManagementService();
         this.candidateRepository = PersistenceContext.repositories().candidates();
+        this.jobOpeningRepository = PersistenceContext.repositories().jobOpenings();
+    }
+
+    public List<JobOpeningDTO> getJobOpeningsList() {
+        return jobOpeningManagementService.getJobOpeningsList();
     }
 
     public boolean registerCandidate(String name, String email, String extension, String number){
         return controller.registerCandidate(name, email, extension, number);
     }
 
-    public Optional<Application> registerApplication(Set<ApplicationFile> files, Date applicationDate, PhoneNumber phone){
+    public Optional<Application> registerApplication(Set<ApplicationFile> files, Date applicationDate, PhoneNumber phone, JobOpeningDTO jobOpeningDTO){
 
+        Optional<Application> application = registerApplication(files, applicationDate, phone);
+
+        if (application.isPresent()){
+            JobOpening jobOpening = jobOpeningManagementService.getJobOpening(jobOpeningDTO);
+
+            Set<Application> orign = jobOpening.getApplications();
+
+            Set<Application> newApplications = new HashSet<>(orign);
+            newApplications.add(application.get());
+
+            jobOpening.setApplications(newApplications);
+            jobOpeningRepository.save(jobOpening);
+        }
+
+        return application;
+    }
+
+    private Optional<Application> registerApplication(Set<ApplicationFile> files, Date applicationDate, PhoneNumber phone){
         authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.OPERATOR);
 
         Optional<Candidate> opCandidate = candidateRepository.findByPhoneNumber(phone);
@@ -55,4 +83,9 @@ public class RegisterJobOpeningApplicationController {
         }
     }
 
+    public boolean getCandidate(PhoneNumber number) {
+        Optional<Candidate> opCandidate = candidateRepository.findByPhoneNumber(number);
+
+        return opCandidate.isPresent();
+    }
 }
