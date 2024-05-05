cab31472a865df5bb5abe02d1325f4d02a64cb96
Ana Guterres <1221933@isep.ipp.pt>
2024-05-05 17:29:47 +0100

    feat: updates us_1012 interview model repository.
    [#74]

--- jobs4u.core/src/main/java/jobs4u/base/interviewmodelmanagement/repositories/InterviewModelRepository.java
+++ jobs4u.core/src/main/java/jobs4u/base/interviewmodelmanagement/repositories/InterviewModelRepository.java
@@ -4,12 +4,11 @@ import eapli.framework.domain.repositories.DomainRepository;
 import jobs4u.base.interviewmodelmanagement.domain.InterviewModel;
 import jobs4u.base.interviewmodelmanagement.domain.InterviewModelName;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface InterviewModelRepository extends DomainRepository<InterviewModelName, InterviewModel> {
 
-    List<InterviewModel> interviewModels();
+    Iterable<InterviewModel> interviewModels();
 
     Optional<InterviewModel> getFileByName(String filename);
 }
