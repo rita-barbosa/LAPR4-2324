5d24fed490c22d52d8755a765b7df7312414ca8a
José Santos - 1220738 <1220738@isep.ipp.pt>
2024-06-02 13:26:55 +0100

    test: Added and modified tests of RecruitmentTest.
    [#130]

--- jobs4u.core/src/test/java/jobs4u/base/recruitmentprocessmanagement/domain/RecruitmentProcessTest.java
+++ jobs4u.core/src/test/java/jobs4u/base/recruitmentprocessmanagement/domain/RecruitmentProcessTest.java
@@ -15,33 +15,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class RecruitmentProcessTest {
 
     @Test
-    void setPhases() {
-
-        List<Phase> listA = new ArrayList<>();
-
-        Calendar start = null;
-        Calendar finish = null;
-
-        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
-        try {
-            start = Calendars.fromDate(df.parse("16-03-2004"));
-            finish = Calendars.fromDate(df.parse("18-03-2024"));
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-
-        RecruitmentProcess recruitmentProcess = new RecruitmentProcess(start, finish, listA);
-
-        Phase phase = new Phase("Test", "Test", "Test", start, finish);
-
-        listA.add(phase);
-
-        recruitmentProcess.setPhases(listA);
-
-        assertTrue(recruitmentProcess.allPhases().contains(phase));
-    }
-
-    @Test
     void ensureItCantCreateWithNullParameter1() {
 
         SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
@@ -57,7 +30,7 @@ class RecruitmentProcessTest {
 
         Calendar finalFinish = finish;
         Calendar finalStart = start;
-        assertThrows(IllegalArgumentException.class, () -> new RecruitmentProcess(null, finalFinish, listA));
+        assertThrows(IllegalArgumentException.class, () -> new RecruitmentProcess(null, finalFinish, listA,new RecruitmentProcessStatus(RecruitmentProcessStatusEnum.PLANNED)));
     }
 
     @Test
@@ -76,7 +49,7 @@ class RecruitmentProcessTest {
 
         Calendar finalFinish = finish;
         Calendar finalStart = start;
-        assertThrows(IllegalArgumentException.class, () -> new RecruitmentProcess(finalStart, null, listA));
+        assertThrows(IllegalArgumentException.class, () -> new RecruitmentProcess(finalStart, null, listA,new RecruitmentProcessStatus(RecruitmentProcessStatusEnum.PLANNED)));
     }
 
     @Test
@@ -95,7 +68,7 @@ class RecruitmentProcessTest {
 
         Calendar finalFinish = finish;
         Calendar finalStart = start;
-        assertThrows(IllegalArgumentException.class, () -> new RecruitmentProcess(finalStart, finalFinish, null));
+        assertThrows(IllegalArgumentException.class, () -> new RecruitmentProcess(finalStart, finalFinish, null,new RecruitmentProcessStatus(RecruitmentProcessStatusEnum.PLANNED)));
     }
 
     @Test
@@ -114,7 +87,7 @@ class RecruitmentProcessTest {
 
         Calendar finalFinish = finish;
         Calendar finalStart = start;
-        assertThrows(IllegalArgumentException.class, () -> new RecruitmentProcess(finalStart, finalFinish, phases));
+        assertThrows(IllegalArgumentException.class, () -> new RecruitmentProcess(finalStart, finalFinish, phases,new RecruitmentProcessStatus(RecruitmentProcessStatusEnum.PLANNED)));
 
     }
 
@@ -134,8 +107,109 @@ class RecruitmentProcessTest {
 
         Calendar finalFinish = finish;
         Calendar finalStart = start;
-        assertThrows(IllegalArgumentException.class, () -> new RecruitmentProcess(finalStart, finalFinish, phases));
+        assertThrows(IllegalArgumentException.class, () -> new RecruitmentProcess(finalStart, finalFinish, phases,new RecruitmentProcessStatus(RecruitmentProcessStatusEnum.PLANNED)));
 
+    }
+
+    @Test
+    void ensureItCantCreateARecruitmentProcessWithOverlappingOrNonSequentialPhases(){
+
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar start1 = null;
+        Calendar finish1 = null;
+        Calendar start2 = null;
+        Calendar finish2 = null;
+        Calendar start3 = null;
+        Calendar finish3 = null;
+        Calendar start4 = null;
+        Calendar finish4 = null;
+        Calendar start5 = null;
+        Calendar finish5 = null;
+        List<Phase> phases = new ArrayList<>();
+        try {
+            start1 = Calendars.fromDate(df.parse("16-03-2024"));
+            finish1 = Calendars.fromDate(df.parse("18-03-2024"));
+            start2 = Calendars.fromDate(df.parse("11-03-2024"));
+            finish2 = Calendars.fromDate(df.parse("13-03-2024"));
+            start3 = Calendars.fromDate(df.parse("20-02-2024"));
+            finish3 = Calendars.fromDate(df.parse("23-02-2024"));
+            start4 = Calendars.fromDate(df.parse("20-03-2024"));
+            finish4 = Calendars.fromDate(df.parse("22-03-2024"));
+            start5 = Calendars.fromDate(df.parse("23-03-2024"));
+            finish5 = Calendars.fromDate(df.parse("24-03-2024"));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        Calendar finalFinish = finish5;
+        Calendar finalStart = start1;
+
+        RecruitmentProcess recruitmentProcess = new RecruitmentProcess(finalStart, finalFinish, phases, new RecruitmentProcessStatus(RecruitmentProcessStatusEnum.PLANNED));
+
+        Phase phase1 = new Phase("Test", "Test", start1, finish1);
+        Phase phase2 = new Phase("Test", "Test", start2, finish2);
+        Phase phase3 = new Phase("Test", "Test", start3, finish3);
+        Phase phase4 = new Phase("Test", "Test", start4, finish4);
+        Phase phase5 = new Phase("Test", "Test", start5, finish5);
+
+        phases.add(phase1);
+        phases.add(phase2);
+        phases.add(phase3);
+        phases.add(phase4);
+        phases.add(phase5);
+
+        assertThrows(IllegalArgumentException.class,() -> recruitmentProcess.addPhases(phases));
+
+    }
+
+    @Test
+    void ensureItCantCreateARecruitmentProcessWithOverlappingOrNonSequentialPhases2(){
+
+        SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar start1 = null;
+        Calendar finish1 = null;
+        Calendar start2 = null;
+        Calendar finish2 = null;
+        Calendar start3 = null;
+        Calendar finish3 = null;
+        Calendar start4 = null;
+        Calendar finish4 = null;
+        Calendar start5 = null;
+        Calendar finish5 = null;
+        List<Phase> phases = new ArrayList<>();
+        try {
+            start1 = Calendars.fromDate(df.parse("16-03-2024"));
+            finish1 = Calendars.fromDate(df.parse("18-03-2024"));
+            start2 = Calendars.fromDate(df.parse("19-03-2024"));
+            finish2 = Calendars.fromDate(df.parse("23-03-2024"));
+            start3 = Calendars.fromDate(df.parse("24-03-2024"));
+            finish3 = Calendars.fromDate(df.parse("29-03-2024"));
+            start4 = Calendars.fromDate(df.parse("01-04-2024"));
+            finish4 = Calendars.fromDate(df.parse("05-04-2024"));
+            start5 = Calendars.fromDate(df.parse("09-04-2024"));
+            finish5 = Calendars.fromDate(df.parse("12-04-2024"));
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        Calendar finalFinish = finish5;
+        Calendar finalStart = start1;
+
+        RecruitmentProcess recruitmentProcess = new RecruitmentProcess(finalStart, finalFinish, phases, new RecruitmentProcessStatus(RecruitmentProcessStatusEnum.PLANNED));
+
+        Phase phase1 = new Phase("Test", "Test", start1, finish1);
+        Phase phase2 = new Phase("Test", "Test", start2, finish2);
+        Phase phase3 = new Phase("Test", "Test", start3, finish3);
+        Phase phase4 = new Phase("Test", "Test", start4, finish4);
+        Phase phase5 = new Phase("Test", "Test", start5, finish5);
+
+        phases.add(phase1);
+        phases.add(phase2);
+        phases.add(phase3);
+        phases.add(phase4);
+        phases.add(phase5);
+
+        assertDoesNotThrow(() -> recruitmentProcess.addPhases(phases));
     }
 
 
