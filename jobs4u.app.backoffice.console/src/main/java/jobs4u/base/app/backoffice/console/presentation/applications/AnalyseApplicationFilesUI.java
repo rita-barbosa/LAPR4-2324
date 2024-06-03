8bfde38869983ca589a47697d406b5f6a773e0ac
Matilde Varela <1220683@isep.ipp.pt>
2024-06-03 15:54:40 +0100

    feat: US4000 UI, Controller, Thread Service implementations
    [#141]

--- jobs4u.app.backoffice.console/src/main/java/jobs4u/base/app/backoffice/console/presentation/applications/AnalyseApplicationFilesUI.java
+++ jobs4u.app.backoffice.console/src/main/java/jobs4u/base/app/backoffice/console/presentation/applications/AnalyseApplicationFilesUI.java
@@ -1,9 +1,14 @@
 package jobs4u.base.app.backoffice.console.presentation.applications;
 
 import eapli.framework.presentation.console.AbstractUI;
+import eapli.framework.presentation.console.SelectWidget;
 import jobs4u.base.applicationmanagement.application.AnalyseApplicationFilesController;
 import jobs4u.base.applicationmanagement.dto.ApplicationDTO;
 import jobs4u.base.jobopeningmanagement.dto.JobOpeningDTO;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.List;
+import java.util.Map;
 
 public class AnalyseApplicationFilesUI extends AbstractUI {
 
@@ -11,36 +16,49 @@ public class AnalyseApplicationFilesUI extends AbstractUI {
 
     @Override
     protected boolean doShow() {
-        controller.getAllJobOpenings();
+        JobOpeningDTO jobOpeningDTO = showAndSelectJobOpening();
+        ApplicationDTO applicationDTO = showAndSelectApplication(jobOpeningDTO.getJobReference());
 
-        String jobReference = "jobReference";
-        controller.getJobOpeningWithJobReference(jobReference);
-
-        ApplicationDTO application = null;
-
-        assert application != null;
-        controller.analyzeFilesFromApplication(application);
-
+        Map<String, Pair<Integer, List<String>>> top20words = controller.analyzeFilesFromApplication(applicationDTO);
+        displayTop20(top20words);
 
         return true;
     }
 
-    @Override
-    public String headline() {
-        return "";
+    private void displayTop20(Map<String, Pair<Integer, List<String>>> top20words) {
+        int orderNumber = 1;
+
+        System.out.println("\n==============================================\n");
+        for (Map.Entry<String, Pair<Integer, List<String>>> entry : top20words.entrySet()) {
+            System.out.println("\n----------------------------\n");
+            System.out.printf("#%d\n[WORD] %s", orderNumber, entry.getKey());
+            System.out.printf(" > [WORD COUNT] %d\n  -> [FILES]\n", entry.getValue().getKey());
+            for (String filename : entry.getValue().getValue()) {
+                System.out.printf("    * %s\n", filename);
+            }
+            System.out.println("----------------------------\n");
+            ++orderNumber;
+        }
+        System.out.println("\n==============================================\n");
     }
 
+    private ApplicationDTO showAndSelectApplication(String jobReference) {
+        SelectWidget<ApplicationDTO> jobOpeningDTOSelectWidget = new SelectWidget<>("Select an Application",
+                controller.getJobOpeningWithJobReference(jobReference));
+        jobOpeningDTOSelectWidget.show();
+        return jobOpeningDTOSelectWidget.selectedElement();
+    }
 
     private JobOpeningDTO showAndSelectJobOpening() {
-//        List<CustomerDTO> customerDTOS = this.controller.getCustomersList();
-//        if (customerDTOS.isEmpty()){
-//            throw new NoSuchElementException("There are no customers to be associated with a job opening.");
-//        }
-//        SelectWidget<CustomerDTO> costumerSelector = new SelectWidget<>("Select a customers that was assigned to you:",
-//                customerDTOS);
-//
-//        costumerSelector.show();
-//        return costumerSelector.selectedElement();
-        throw new UnsupportedOperationException("Not supported yet.");
+        SelectWidget<JobOpeningDTO> applicationDTOSelectWidget = new SelectWidget<>("Select a Job Opening",
+                controller.getAllJobOpenings());
+        applicationDTOSelectWidget.show();
+        return applicationDTOSelectWidget.selectedElement();
     }
+
+    @Override
+    public String headline() {
+        return "Top 20 Used Words";
+    }
+
 }
