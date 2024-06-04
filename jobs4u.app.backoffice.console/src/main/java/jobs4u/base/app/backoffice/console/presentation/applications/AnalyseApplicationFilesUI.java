fda3f9f88cdab7821d127f03c27146c6418df7d3
José Santos - 1220738 <1220738@isep.ipp.pt>
2024-06-04 15:50:39 +0100

    feat : Changed certain methods of the UI to better present the data to the user.
    [#137][#141]

--- jobs4u.app.backoffice.console/src/main/java/jobs4u/base/app/backoffice/console/presentation/applications/AnalyseApplicationFilesUI.java
+++ jobs4u.app.backoffice.console/src/main/java/jobs4u/base/app/backoffice/console/presentation/applications/AnalyseApplicationFilesUI.java
@@ -3,7 +3,9 @@ package jobs4u.base.app.backoffice.console.presentation.applications;
 import eapli.framework.presentation.console.AbstractUI;
 import eapli.framework.presentation.console.SelectWidget;
 import jobs4u.base.applicationmanagement.application.AnalyseApplicationFilesController;
+import jobs4u.base.applicationmanagement.domain.ApplicationFile;
 import jobs4u.base.applicationmanagement.dto.ApplicationDTO;
+import jobs4u.base.candidatemanagement.dto.CandidateDTO;
 import jobs4u.base.jobopeningmanagement.dto.JobOpeningDTO;
 import org.apache.commons.lang3.tuple.Pair;
 
@@ -16,11 +18,31 @@ public class AnalyseApplicationFilesUI extends AbstractUI {
 
     @Override
     protected boolean doShow() {
-        JobOpeningDTO jobOpeningDTO = showAndSelectJobOpening();
-        ApplicationDTO applicationDTO = showAndSelectApplication(jobOpeningDTO.getJobReference());
+        CandidateDTO candidateDTO = showAndSelectCandidate();
 
-        Map<String, Pair<Integer, List<String>>> top20words = controller.analyzeFilesFromApplication(applicationDTO);
-        displayTop20(top20words);
+        System.out.println("----------------------- INFO FROM CANDIDATE -----------------------");
+        System.out.println("NAME : "+candidateDTO.getCandidateName());
+        System.out.println("PHONE NUMBER : "+candidateDTO.getCandidatePhoneNumber());
+        System.out.println("EMAIL : "+candidateDTO.getCandidateEmail());
+        System.out.println("--------------------------- APPLICATIONS ---------------------------");
+
+        Iterable<ApplicationDTO> list = controller.getAllApplicationsDTOByCandidate(candidateDTO.getCandidatePhoneNumber());
+        int i = 0;
+
+        for (ApplicationDTO applicationDTO : list){
+            System.out.printf("--------------------------------- %d ---------------------------------\n", i+1);
+            System.out.println("ID : "+applicationDTO.getId());
+            System.out.println("CANDIDATE : "+applicationDTO.getCandidate());
+            System.out.println("STATUS : "+applicationDTO.getApplicationStatus());
+            System.out.printf("FILES : ");
+            for (ApplicationFile applicationFile : applicationDTO.getApplicationFiles()){
+                System.out.printf(applicationFile.getApplicationFile()+" | ");
+            }
+            System.out.println();
+            Map<String, Pair<Integer, List<String>>> top20words = controller.analyzeFilesFromApplication(applicationDTO);
+            displayTop20(top20words);
+            i++;
+        }
 
         return true;
     }
@@ -28,32 +50,23 @@ public class AnalyseApplicationFilesUI extends AbstractUI {
     private void displayTop20(Map<String, Pair<Integer, List<String>>> top20words) {
         int orderNumber = 1;
 
-        System.out.println("\n==============================================\n");
+        System.out.println("--------------------------- TOP 20 WORDS ---------------------------");
         for (Map.Entry<String, Pair<Integer, List<String>>> entry : top20words.entrySet()) {
-            System.out.println("\n----------------------------\n");
-            System.out.printf("#%d\n[WORD] %s", orderNumber, entry.getKey());
-            System.out.printf(" > [WORD COUNT] %d\n  -> [FILES]\n", entry.getValue().getKey());
-            for (String filename : entry.getValue().getValue()) {
-                System.out.printf("    * %s\n", filename);
+            System.out.printf("#%d [WORD] '%s'", orderNumber, entry.getKey());
+            System.out.printf(" > [WORD COUNT] %d\n  -> [FILES] ", entry.getValue().getKey());
+            for (String filename : entry.getValue().getRight()) {
+                System.out.printf("%s | ", filename);
             }
-            System.out.println("----------------------------\n");
+            System.out.println("\n--------------------------------------------------------------------");
             ++orderNumber;
         }
-        System.out.println("\n==============================================\n");
     }
 
-    private ApplicationDTO showAndSelectApplication(String jobReference) {
-        SelectWidget<ApplicationDTO> jobOpeningDTOSelectWidget = new SelectWidget<>("Select an Application",
-                controller.getJobOpeningWithJobReference(jobReference));
-        jobOpeningDTOSelectWidget.show();
-        return jobOpeningDTOSelectWidget.selectedElement();
-    }
-
-    private JobOpeningDTO showAndSelectJobOpening() {
-        SelectWidget<JobOpeningDTO> applicationDTOSelectWidget = new SelectWidget<>("Select a Job Opening",
-                controller.getAllJobOpenings());
-        applicationDTOSelectWidget.show();
-        return applicationDTOSelectWidget.selectedElement();
+    private CandidateDTO showAndSelectCandidate() {
+        SelectWidget<CandidateDTO> candidateDTOSelectWidget = new SelectWidget<>("Select a Candidate",
+                controller.getAllCandidatesDTO());
+        candidateDTOSelectWidget.show();
+        return candidateDTOSelectWidget.selectedElement();
     }
 
     @Override
