2bb85fdeab78dea9284da9cbfc397a8b1c9a3826
José Santos - 1220738 <1220738@isep.ipp.pt>
2024-06-04 15:51:18 +0100

    feat : Changed certain methods of the Controller to present the correct data to the user following a clarification with the client.
    [#137][#141]

--- jobs4u.core/src/main/java/jobs4u/base/applicationmanagement/application/AnalyseApplicationFilesController.java
+++ jobs4u.core/src/main/java/jobs4u/base/applicationmanagement/application/AnalyseApplicationFilesController.java
@@ -2,14 +2,9 @@ package jobs4u.base.applicationmanagement.application;
 
 import eapli.framework.infrastructure.authz.application.AuthorizationService;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
-import eapli.framework.infrastructure.authz.application.UserSession;
-import eapli.framework.infrastructure.authz.domain.model.Username;
-import jobs4u.base.applicationmanagement.domain.Application;
 import jobs4u.base.applicationmanagement.dto.ApplicationDTO;
-import jobs4u.base.jobopeningmanagement.application.JobOpeningManagementService;
-import jobs4u.base.jobopeningmanagement.domain.JobOpening;
-import jobs4u.base.jobopeningmanagement.dto.JobOpeningDTO;
-import jobs4u.base.usermanagement.domain.BaseRoles;
+import jobs4u.base.candidatemanagement.application.CandidateManagementService;
+import jobs4u.base.candidatemanagement.dto.CandidateDTO;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.*;
@@ -17,36 +12,27 @@ import java.util.*;
 public class AnalyseApplicationFilesController {
 
     AuthorizationService authorizationService;
-    JobOpeningManagementService jobOpeningManagementService;
+    ApplicationManagementService applicationManagementService;
     ApplicationFilesThreadService applicationFilesThreadService;
+    CandidateManagementService candidateManagementService;
 
     public AnalyseApplicationFilesController() {
-        jobOpeningManagementService = new JobOpeningManagementService();
+        candidateManagementService = new CandidateManagementService();
         applicationFilesThreadService = new ApplicationFilesThreadService();
         authorizationService = AuthzRegistry.authorizationService();
+        applicationManagementService = new ApplicationManagementService();
     }
 
-    public Iterable<JobOpeningDTO> getAllJobOpenings() {
-        authorizationService.ensureAuthenticatedUserHasAnyOf(BaseRoles.CUSTOMER_MANAGER);
-        Optional<UserSession> session = authorizationService.session();
-        if (session.isPresent()) {
-            Username username = session.get().authenticatedUser().username();
-            return jobOpeningManagementService.jobOpeningsOfCustomerManager(username);
-        }
-        throw new NoSuchElementException("Could not find user in session");
-    }
-
-    public List<ApplicationDTO> getJobOpeningWithJobReference(String jobReference) {
-        Optional<JobOpening> jobOpening = jobOpeningManagementService.getJobOpeningByJobRef(jobReference);
-        List<ApplicationDTO> applicationDTOList = new ArrayList<>();
-        assert jobOpening.isPresent();
-        for (Application application : jobOpening.get().getApplications()) {
-            applicationDTOList.add(application.toDTO());
-        }
-        return applicationDTOList;
-    }
 
     public Map<String, Pair<Integer, List<String>>> analyzeFilesFromApplication(ApplicationDTO application) {
-        return applicationFilesThreadService.getTop20Words(application.getApplicationFiles());
+        return applicationFilesThreadService.getTop20Words(applicationManagementService.getApplicationWithId(application.getId()).get().allFiles());
+    }
+
+    public Iterable<CandidateDTO> getAllCandidatesDTO() {
+        return candidateManagementService.getCandidatesListDTO();
+    }
+
+    public Iterable<ApplicationDTO> getAllApplicationsDTOByCandidate(String candidatePhoneNumber) {
+        return applicationManagementService.getAllApplicationsThatHaveCandidate(candidatePhoneNumber);
     }
 }
