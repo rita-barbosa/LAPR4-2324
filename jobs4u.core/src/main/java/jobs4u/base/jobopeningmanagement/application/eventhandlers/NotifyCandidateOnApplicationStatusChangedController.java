d422d462d18ead039c752d87ec4842e17181bd41
RitaBarbosa-1220841 <1220841@isep.ipp.pt>
2024-06-05 23:27:08 +0100

    feat: adds events controller classes related to us1020. [#180]

---
+++ jobs4u.core/src/main/java/jobs4u/base/jobopeningmanagement/application/eventhandlers/NotifyCandidateOnApplicationStatusChangedController.java
@@ -0,0 +1,8 @@
+package jobs4u.base.jobopeningmanagement.application.eventhandlers;
+
+import jobs4u.base.jobopeningmanagement.domain.events.ApplicationStatusChangedEvent;
+
+public class NotifyCandidateOnApplicationStatusChangedController {
+    public void notifyCandidateOfChangedStatus(ApplicationStatusChangedEvent event) {
+    }
+}
