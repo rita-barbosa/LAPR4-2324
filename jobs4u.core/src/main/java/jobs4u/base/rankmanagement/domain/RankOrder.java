28fa3d65e4015df4da9b4137039e9e9e66dbcedf
RitaBarbosa-1220841 <1220841@isep.ipp.pt>
2024-06-05 23:26:00 +0100

    feat: updates RankOrder and RankRepository. [#180]

--- jobs4u.core/src/main/java/jobs4u/base/rankmanagement/domain/RankOrder.java
+++ jobs4u.core/src/main/java/jobs4u/base/rankmanagement/domain/RankOrder.java
@@ -1,8 +1,12 @@
 package jobs4u.base.rankmanagement.domain;
 
 import eapli.framework.domain.model.ValueObject;
+import eapli.framework.general.domain.model.EmailAddress;
 import jakarta.persistence.*;
+
+import jobs4u.base.applicationmanagement.domain.Application;
 import jobs4u.base.candidatemanagement.domain.Candidate;
+import jobs4u.base.jobopeningmanagement.domain.JobReference;
 
 @Entity
 @Table(name = "T_RANK_ENTRIES")
@@ -13,7 +17,8 @@ public class RankOrder implements ValueObject {
     private Integer rankEntryId;
 
     @ManyToOne
-    private Candidate candidate;
+    @JoinColumn(name = "application_id")
+    private Application application;
 
     private Integer numberRanked;
 
@@ -21,13 +26,26 @@ public class RankOrder implements ValueObject {
         //for ORM
     }
 
-    public RankOrder(Candidate candidate, Integer numberRanked) {
-        this.candidate = candidate;
+    public RankOrder(Application application, Integer numberRanked) {
+        this.application = application;
         this.numberRanked = numberRanked;
     }
 
-    public Integer numberRanked(){
+    public Integer numberRanked() {
         return this.numberRanked;
     }
 
+    public Application acceptApplication() {
+        this.application.acceptApplication();
+        return this.application;
+    }
+
+    public EmailAddress candidateEmail() {
+        return this.application.candidate().email();
+    }
+
+    public Application rejectApplication() {
+        this.application.rejectApplication();
+        return this.application;
+    }
 }
--- jobs4u.core/src/main/java/jobs4u/base/rankmanagement/persistence/RankRepository.java
+++ jobs4u.core/src/main/java/jobs4u/base/rankmanagement/persistence/RankRepository.java
@@ -1,4 +1,8 @@
 package jobs4u.base.rankmanagement.persistence;
 
-public interface RankRepository {
+import eapli.framework.domain.repositories.DomainRepository;
+import jobs4u.base.jobopeningmanagement.domain.JobReference;
+import jobs4u.base.rankmanagement.domain.Rank;
+
+public interface RankRepository extends DomainRepository<JobReference, Rank> {
 }
