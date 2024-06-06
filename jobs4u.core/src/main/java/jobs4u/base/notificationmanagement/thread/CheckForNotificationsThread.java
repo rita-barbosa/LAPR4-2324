435a4e577bc9c223734776ed6a6eb03017a0d504
José Santos - 1220738 <1220738@isep.ipp.pt>
2024-06-07 00:30:58 +0100

    feat : Added a specialized thread to constantly check for new notifications.
    [#184][#198][#225]

---
+++ jobs4u.core/src/main/java/jobs4u/base/notificationmanagement/thread/CheckForNotificationsThread.java
@@ -0,0 +1,49 @@
+package jobs4u.base.notificationmanagement.thread;
+
+import eapli.framework.infrastructure.authz.domain.model.Username;
+import eapli.framework.time.util.Calendars;
+import jobs4u.base.network.FollowUpConnectionService;
+import jobs4u.base.network.SerializationUtil;
+import jobs4u.base.network.data.DataDTO;
+import jobs4u.base.notificationmanagement.dto.NotificationDTO;
+import jobs4u.base.notificationmanagement.application.NotificationManagementService;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.time.format.DateTimeParseException;
+import java.util.Calendar;
+
+//TODO ADD LOGGER COMMENTS
+public class CheckForNotificationsThread implements Runnable {
+
+    public final Username username;
+
+    private final FollowUpConnectionService followUpConnectionService;
+
+    public CheckForNotificationsThread(Username username) {
+        this.username = username;
+        this.followUpConnectionService = new FollowUpConnectionService();
+    }
+
+    @Override
+    public void run() {
+        String answer;
+
+        while(true) {
+
+            answer = (String) SerializationUtil.deserialize(followUpConnectionService.startCheckForNotificationsThread(username).dataBlockList().get(0).data());
+
+            if(answer.equals("true")){
+                System.out.println("\n[NEW NOTIFICATIONS TO BE SEEN]");
+            }
+
+            try {
+                Thread.sleep(30000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+}
