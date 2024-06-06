d9745e871f4ce64a7a1d583157702aaa2cfc6e75
Ana Guterres <1221933@isep.ipp.pt>
2024-06-06 20:37:40 +0100

    feat: adds method to InterviewModelManagementService related to us1017.
    [#203]

--- jobs4u.core/src/main/java/jobs4u/base/interviewmodelmanagement/application/InterviewModelManagementService.java
+++ jobs4u.core/src/main/java/jobs4u/base/interviewmodelmanagement/application/InterviewModelManagementService.java
@@ -5,12 +5,20 @@ import jobs4u.base.interviewmodelmanagement.domain.InterviewModelName;
 import jobs4u.base.interviewmodelmanagement.dto.InterviewModelDTO;
 import jobs4u.base.interviewmodelmanagement.domain.InterviewModel;
 import jobs4u.base.interviewmodelmanagement.repositories.InterviewModelRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import plugins.FileManagement;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
 
 public class InterviewModelManagementService {
 
     private static final InterviewModelRepository repository = PersistenceContext.repositories().interviewModels();
 
     private final InterviewModelDtoService dtoService = new InterviewModelDtoService();
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersistenceContext.class);
 
     public Iterable<InterviewModelDTO> allInterviewModels() {
          return dtoService.toDto(repository.findAll());
@@ -25,4 +33,21 @@ public class InterviewModelManagementService {
         return repository.containsOfIdentity(new InterviewModelName(identifier));
     }
 
+    public boolean checkAnswersFileIsValid(String interviewName, String filepath) {
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
+        try {
+            Optional<InterviewModel> im = repository.getFileByName(interviewName);
+            if (im.isPresent()) {
+                InterviewModel interviewSpec = im.get();
+                FileManagement dataImporterInstance = (FileManagement) loader.loadClass(interviewSpec.dataImporter()).getDeclaredConstructor().newInstance();
+                dataImporterInstance.importData(interviewSpec.configurationFile().toString());
+                return dataImporterInstance.checkFileFormat(filepath);
+            }
+        } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException |
+                 InvocationTargetException e) {
+            LOGGER.error("Unable to access plugin.");
+            return false;
+        }
+        return false;
+    }
 }
