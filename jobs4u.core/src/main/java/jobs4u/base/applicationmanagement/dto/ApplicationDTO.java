9a01fdc548db2e169ce80455586a820d5067dafe
Ana Guterres <1221933@isep.ipp.pt>
2024-06-07 07:29:01 +0100

    feat: updates ApplicationDTO class to include Interview and Requirements related to us1019.
    [#209]

--- jobs4u.core/src/main/java/jobs4u/base/applicationmanagement/dto/ApplicationDTO.java
+++ jobs4u.core/src/main/java/jobs4u/base/applicationmanagement/dto/ApplicationDTO.java
@@ -1,6 +1,9 @@
 package jobs4u.base.applicationmanagement.dto;
 
 import jobs4u.base.applicationmanagement.domain.ApplicationFile;
+import jobs4u.base.applicationmanagement.domain.Interview;
+import jobs4u.base.applicationmanagement.domain.RequirementAnswer;
+import jobs4u.base.applicationmanagement.domain.RequirementResult;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -14,15 +17,33 @@ public class ApplicationDTO implements Serializable {
     private final Set<ApplicationFile> files;
     private final Date applicationDate;
     private final String applicationStatus;
+    private final String candidateName;
     private final String candidate;
+    private final Interview interview;
+    private final RequirementAnswer requirementAnswer;
+    private final RequirementResult requirementResult;
 
-    public ApplicationDTO(Long id, Set<ApplicationFile> files, Date applicationDate, String applicationStatus, String candidate) {
+
+    public ApplicationDTO(Long id, Set<ApplicationFile> files, Date applicationDate, String applicationStatus,String candidateName, String candidate, Interview interview, RequirementAnswer requirementAnswer, RequirementResult requirementResult) {
         this.id = id;
         this.files = files;
         this.applicationDate = applicationDate;
         this.applicationStatus = applicationStatus;
+        this.candidateName = candidateName;
         this.candidate = candidate;
+        this.interview = interview;
+        this.requirementAnswer = requirementAnswer;
+        this.requirementResult = requirementResult;
     }
+
+//    public ApplicationDTO(Long id, Set<ApplicationFile> files, Date applicationDate, String applicationStatus, String candidate, Interview interview) {
+//        this.id = id;
+//        this.files = files;
+//        this.applicationDate = applicationDate;
+//        this.applicationStatus = applicationStatus;
+//        this.candidate = candidate;
+//        this.interview = interview;
+//    }
 
 
     @Override
@@ -32,10 +53,11 @@ public class ApplicationDTO implements Serializable {
                         "#File: %s\n" +
                         "#Application Date: %s\n" +
                         "#Application Status: %s\n" +
+                        "#Candidate name: %s\n" +
                         "#Candidate username: %s\n" +
                         "=====================================================================\n",
                 id, files, applicationDate,
-                applicationStatus, candidate);
+                applicationStatus, candidateName, candidate);
     }
 
     public Set<ApplicationFile> getApplicationFiles() {
@@ -56,5 +78,21 @@ public class ApplicationDTO implements Serializable {
 
     public Long getId() {
         return id;
+    }
+
+    public Interview getInterview(){
+        return interview;
+    }
+
+    public RequirementAnswer getRequirementAnswer(){
+        return requirementAnswer;
+    }
+
+    public RequirementResult getRequirementResult(){
+        return requirementResult;
+    }
+
+    public String getCandidateName(){
+        return candidateName;
     }
 }
