30e855d117d67e61f6479fd90dd43a9917b1cc8d
Ana Guterres <1221933@isep.ipp.pt>
2024-06-07 07:35:33 +0100

    feat: adds Controller class for us1021.
    [#214]

---
+++ jobs4u.core/src/main/java/jobs4u/base/applicationmanagement/application/DisplayAllApplicationDataController.java
@@ -0,0 +1,38 @@
+package jobs4u.base.applicationmanagement.application;
+
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.infrastructure.authz.domain.model.SystemUser;
+import jobs4u.base.applicationmanagement.dto.ApplicationDTO;
+import jobs4u.base.jobopeningmanagement.application.JobOpeningManagementService;
+import jobs4u.base.jobopeningmanagement.dto.JobOpeningDTO;
+import jobs4u.base.usermanagement.domain.BaseRoles;
+
+import java.util.List;
+import java.util.Optional;
+
+public class DisplayAllApplicationDataController {
+
+    private final AuthorizationService authz;
+
+    private final JobOpeningManagementService jobOpeningManagementService;
+
+
+    public DisplayAllApplicationDataController(){
+        this.authz = AuthzRegistry.authorizationService();
+        this.jobOpeningManagementService = new JobOpeningManagementService();
+    }
+
+    public List <ApplicationDTO> getApplicationsList(){
+        Iterable<JobOpeningDTO> jobOpeningDTOList = getJobOpeningList();
+
+        return jobOpeningManagementService.getApplicationsList(jobOpeningDTOList);
+    }
+
+
+    public Iterable<JobOpeningDTO> getJobOpeningList() {
+        Optional<SystemUser> customerManager = authz.loggedinUserWithPermissions(BaseRoles.CUSTOMER_MANAGER);
+        return customerManager.map(systemUser -> jobOpeningManagementService.jobOpeningsInScreeingListOfCustomerManager(systemUser.username())).orElse(null);
+
+    }
+}
