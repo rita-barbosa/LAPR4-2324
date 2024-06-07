7e10efb025bfb2747634fe417b448cd86c4d64c2
Vitor Fardilha <1211273@isep.ipp.pt>
2024-06-07 17:16:12 +0100

    docs:Main Menu implementation[#195]

--- jobs4u.app.backoffice.console/src/main/java/jobs4u/base/app/backoffice/console/presentation/MainMenu.java
+++ jobs4u.app.backoffice.console/src/main/java/jobs4u/base/app/backoffice/console/presentation/MainMenu.java
@@ -34,6 +34,7 @@ import jobs4u.base.app.backoffice.console.presentation.customer.RegisterCustomer
 import jobs4u.base.app.backoffice.console.presentation.interviewTemplate.GenerateInterviewModelTemplateFileUI;
 import jobs4u.base.app.backoffice.console.presentation.interviewmodel.SelectInterviewModelAction;
 import jobs4u.base.app.backoffice.console.presentation.jobopening.editing.EditJobOpeningAction;
+import jobs4u.base.app.backoffice.console.presentation.jobopening.editing.EvaluateInterviewsAction;
 import jobs4u.base.app.backoffice.console.presentation.jobopening.listing.ListJobOpeningsAction;
 import jobs4u.base.app.backoffice.console.presentation.jobopening.publishing.PublishResultJobOpeningAction;
 import jobs4u.base.app.backoffice.console.presentation.jobopening.registration.RegisterJobOpeningAction;
@@ -273,6 +274,8 @@ public class MainMenu extends AbstractUI {
         menu.addItem(9, "Verify Requirement Specifitions of applications", new VerifyRequirementAction());
         menu.addItem(10,"Send Notification Email with Requirement Specification result", new SendNotificationEmailAction());
         menu.addItem(11,"Publish results of job opening", new PublishResultJobOpeningAction());
+        menu.addItem(12, "Evaluate interviews for a job opening", new EvaluateInterviewsAction());
+
         menu.addItem(EXIT_OPTION, RETURN_LABEL, Actions.SUCCESS);
 
         return menu;
