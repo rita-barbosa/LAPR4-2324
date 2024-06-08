83904024386e26cb56ced7128302edc07f9b6e18
José Santos - 1220738 <1220738@isep.ipp.pt>
2024-06-08 13:43:19 +0100

    feat : Changed implementation of the events to make sure only the necessary amount of events was used
    close #269

--- jobs4u.core/src/main/java/jobs4u/base/applicationmanagement/application/eventhandlers/NotifyCandidateOnApplicationStatusChangedController.java
+++ jobs4u.core/src/main/java/jobs4u/base/applicationmanagement/application/eventhandlers/NotifyCandidateOnApplicationStatusChangedController.java
@@ -2,8 +2,6 @@ package jobs4u.base.applicationmanagement.application.eventhandlers;
 
 import jobs4u.base.infrastructure.persistence.PersistenceContext;
 import jobs4u.base.jobopeningmanagement.application.JobOpeningManagementService;
-import jobs4u.base.jobopeningmanagement.domain.events.ApplicationAcceptedEvent;
-import jobs4u.base.jobopeningmanagement.domain.events.ApplicationReceivedEvent;
 import jobs4u.base.jobopeningmanagement.domain.events.ApplicationStatusChangedEvent;
 import jobs4u.base.notificationmanagement.application.NotificationManagementService;
 import jobs4u.base.notificationmanagement.domain.*;
@@ -26,23 +24,4 @@ public class NotifyCandidateOnApplicationStatusChangedController {
         notificationManagementService.saveNotification(notification);
     }
 
-    public void notifyCandidateOfAcceptedApplication(ApplicationAcceptedEvent event) {
-        Notification notification = new Notification(new NotificationType(String.valueOf(NotificationTypeEnum.CANDIDATE)),
-                new NotificationRecipient(event.receiverEmail().toString()),
-                new NotificationBody(String.format("YOUR APPLICATION FOR THE JOB OF %s AT %s WAS ACCEPTED.",jobOpeningManagementService.getJobOpeningByJobRef(event.jobReference().toString()).get().function(), event.jobReference().getCompanyCode())),
-                new NotificationDate(Calendar.getInstance()),
-                new NotificationStatus(String.valueOf(NotificationStatusEnum.UNSEEN)));
-
-        notificationManagementService.saveNotification(notification);
-    }
-
-    public void notifyCandidateOfReceived(ApplicationReceivedEvent event) {
-        Notification notification = new Notification(new NotificationType(String.valueOf(NotificationTypeEnum.CANDIDATE)),
-                new NotificationRecipient(event.getEmailAddress().toString()),
-                new NotificationBody(String.format("YOUR APPLICATION FOR THE JOB OF %s AT %s WAS RECEIVED.",jobOpeningManagementService.getJobOpeningByJobRef(event.getJobReference().toString()).get().function(), event.getJobReference())),
-                new NotificationDate(Calendar.getInstance()),
-                new NotificationStatus(String.valueOf(NotificationStatusEnum.UNSEEN)));
-
-        notificationManagementService.saveNotification(notification);
-    }
 }
